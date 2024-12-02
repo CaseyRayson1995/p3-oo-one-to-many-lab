fa622d5cf6e48580667c1388b2c091594a75ca10
CaseyRayson1995 <caseyrayson1995@gmail.com>
2024-12-02 09:01:48 -0500

    one to many

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,55 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
+    @property
+    def pet_type(self):
+        return self._pet_type
+
+    @pet_type.setter
+    def pet_type(self, value):
+        if value not in Pet.PET_TYPES:
+            raise Exception(f"Invalid pet type: {value}. Must be one of {Pet.PET_TYPES}.")
+        self._pet_type = value
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, value):
+        if value is not None and not isinstance(value, Owner):
+            raise Exception("Owner must be an instance of the Owner class.")
+        self._owner = value
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise Exception("Name must be a string.")
+        self._name = value
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Pet must be an instance of the Pet class.")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
